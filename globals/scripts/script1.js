69cd6e983685df96d15197ec7c6d5b9f4e2569c6
bokspro <bokspro@gmail.com>
2018-06-10 17:32:52 +0300

    Add files via upload

---
+++ globals/scripts/script1.js
@@ -0,0 +1,224 @@
+var LatLon = {
+    x: 0.0,
+    y: 0.0
+};
+var lines = {
+    x1: 0.0,
+    y1: 0.0,
+    x2: 0.0,
+    y2: 0.0,
+    z: 1.0
+};
+var width;
+var height;
+// X and Y boundaries
+var westLong = 20.967488;
+var eastLong = 28.242923;
+var northLat = 58.085900;
+var southLat = 55.674427;
+
+var background = new Image();
+background.src = "latvija.png";
+var body = document.getElementById('body');
+
+var began=false;
+RAD2DEG = 180 / Math.PI;
+PI_4 = Math.PI / 4;
+
+var tracker;
+
+var myAppArea = {
+    canvas : document.getElementById("myCanvas"),
+    start : function() {
+        this.canvas.height = body.offsetHeight*2;
+        this.canvas.width = body.offsetWidth*2;
+        this.context = this.canvas.getContext("2d");
+        this.frameNo = 0;
+        this.interval = setInterval(updateCanvas, 3000);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+function startApp() {
+    width = 4095;
+    height = 2477;
+    myAppArea.start();
+    var imageWidth = 0;
+    var imageHeight = 0;
+
+        if(background.complete){
+            if (width/height > body.offsetWidth/body.offsetHeight) {
+                imageWidth = body.offsetWidth;
+                imageHeight = background.height * (imageWidth / background.width);//parseInt(background.height * (imageWidth / background.width));
+            console.log('width');
+            } else {
+                imageHeight = body.offsetHeight;
+                imageWidth = background.width * (imageHeight / background.height);//parseInt(background.width * (imageHeight / background.height));
+            console.log('height');
+            }
+
+            console.log(imageWidth);
+            console.log(imageHeight);
+
+            myAppArea.context.drawImage(
+                background,
+                0,
+                0,
+                imageWidth*2,
+                imageHeight*2
+            );
+            width=imageWidth*2;
+            height=imageHeight*2;
+            lines.z=height;
+            // } else {
+            //     background.onload = function(){
+            //         myAppArea.context.drawImage(
+            //             background,
+            //             0,
+            //             0,
+            //             imageWidth,
+            //             imageHeight
+            //         );
+            //     };
+            // }
+            //localStorage.clear();
+            //updateCanvas();
+            if(localStorage.length){        
+                for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+
+                    pos = ( JSON.parse(localStorage.getItem( localStorage.key( i ) ) ) );
+                    myAppArea.context.strokeStyle="#FF0000";
+                    myAppArea.context.beginPath();
+                    myAppArea.context.moveTo(pos.x1*height/pos.z,pos.y1*height/pos.z);
+                    myAppArea.context.lineTo(pos.x2*height/pos.z,pos.y2*height/pos.z);
+                    myAppArea.context.stroke();
+                }
+            }
+            loadCitys();
+            myAppArea.context.beginPath();
+            getLocation();
+    }
+}
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else { 
+        wrapText(myAppArea.context,"Geolocation is not supported by this browser.",width/2-100,height/2-15,width,300);
+    }
+}
+
+function showPosition(position) {
+    var lon=position.coords.longitude;
+    var lat=position.coords.latitude;
+    LatLon.x = width * ((lon-westLong)/(eastLong-westLong)); 
+    LatLon.y = height * ((northLat-lat)/(northLat-southLat));
+    if(began==true){
+
+         //bauska
+        lines.x2 = LatLon.x; 
+        lines.y2 = LatLon.y;
+        localStorage.setItem(localStorage.length, JSON.stringify(lines));
+        myAppArea.context.lineTo(LatLon.x,LatLon.y);
+        myAppArea.context.stroke();
+        //myAppArea.context.fillRect(LatLon.x-2, LatLon.y-2, 4, 4);
+        myAppArea.context.beginPath();
+        myAppArea.context.moveTo(LatLon.x,LatLon.y);
+        lines.x1 = LatLon.x; 
+        lines.y1 = LatLon.y;
+    }else{
+        myAppArea.context.moveTo(LatLon.x,LatLon.y);
+        lines.x1 = LatLon.x; 
+        lines.y1 = LatLon.y;
+        began=true;
+    }
+}
+function wrapText(context, text, x, y, maxWidth, lineHeight) {
+var words = text.split(' ');
+var line = '';
+
+for(var n = 0; n < words.length; n++) {
+  var testLine = line + words[n] + ' ';
+  var metrics = context.measureText(testLine);
+  var testWidth = metrics.width;
+  if (testWidth > maxWidth && n > 0) {
+    context.fillText(line, x, y);
+    line = words[n] + ' ';
+    y += lineHeight;
+  }
+  else {
+    line = testLine;
+  }
+}
+context.fillText(line, x, y);
+}
+
+function showError(error) {
+    myAppArea.context.font = "30px Arial";
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            wrapText(myAppArea.context,"User denied the request for Geolocation.",width/2-100,height/2-15,width,300);
+
+            break;
+        case error.POSITION_UNAVAILABLE:
+            wrapText(myAppArea.context,"Location information is unavailable.",width/2-100,height/2-15,width,300);
+
+            break;
+        case error.TIMEOUT:
+            wrapText(myAppArea.context,"The request to get user location timed out.",width/2-100,height/2-15,width,300);
+
+            break;
+        case error.UNKNOWN_ERROR:
+            wrapText(myAppArea.context,"An unknown error occurred.",width/2-100,height/2-15,width,300);
+           
+            break;
+    }
+}
+
+function getDistanceFromLatLon(lat1, lon1, lat2, lon2){  // generally used geo measurement function
+    var R = 6378.137; // Radius of earth in KM    
+    var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+    var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+    Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d * 1000; // meters
+}
+
+
+
+function updateCanvas(){
+     // talsi
+        //localStorage.setItem("vardi", visi);
+        /*
+            for (var key in localStorage){
+                console.log(key)
+            }
+            JSON.stringify(users);
+            JSON.parse(localStorage['users']);
+            https://www.youtube.com/watch?v=RbfG7NLKDgQ
+        */
+       localStorage.clear();
+
+    /*lines.x1 = width * ((22.531332-westLong)/(eastLong-westLong)); 
+    lines.y1 = height * ((northLat-57.320654)/(northLat-southLat));
+     //bauska
+    lines.x2 = width * ((24.861906-westLong)/(eastLong-westLong)); 
+    lines.y2 = height * ((northLat-56.539413)/(northLat-southLat));
+    localStorage.setItem(localStorage.length, JSON.stringify(lines));
+    for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+        console.log( JSON.parse(localStorage.getItem( localStorage.key( i ) ) ) );
+    }*/
+    getLocation();
+    //console.log('x: '+ LatLon.x + ' y: ' +LatLon.y);
+
+
+
+   
+   // getLocation();
+    //console.log('x: '+ LatLon.x + ' y: ' +LatLon.y);
+    //myAppArea.context.fillStyle = "#FF0000";
+    //myAppArea.context.fillRect(LatLon.x-10,LatLon.y-10,20,20);
+}
---
+++ globals/scripts/script2.js
@@ -0,0 +1,31 @@
+var citys = [
+	{city: 'Riga', pops: 701185, lat: 56.9475, lon: 24.106389},
+	{city: 'Daugavpils', pops: 98089, lat: 55.865348, lon: 26.562868},
+	{city: 'Liepaja', pops: 79995, lat: 56.502294, lon: 21.019955},
+	{city: 'Jalgava', pops: 62572, lat: 56.639862, lon: 23.725242},
+	{city: 'Jurmala', pops: 57385, lat: 56.947968, lon: 23.618976},
+	{city: 'Ventspils', pops: 40679, lat: 57.401730, lon: 21.576574},
+	{city: 'Rezekne', pops: 32630, lat: 56.484768, lon: 27.346534},
+	{city: 'Ogre', pops: 25894, lat: 56.803410, lon: 24.621935}
+];
+
+function getPos(lon,lat){
+	LatLon.x = width * ((lon-westLong)/(eastLong-westLong)); 
+	LatLon.y = height * ((northLat-lat)/(northLat-southLat));
+}
+function loadCitys(){
+	for(var i=0,l=citys.length;i<l;i++){
+		getPos(citys[i].lon,citys[i].lat);
+		myAppArea.context.fillStyle = 'green';
+        myAppArea.context.beginPath();
+        myAppArea.context.arc(LatLon.x,LatLon.y,Math.sqrt((citys[i].pops/1000000)*3000),0,2*Math.PI);
+        myAppArea.context.strokeStyle='yellow';
+        if(this.alpha==true){
+            myAppArea.context.strokeStyle="blue";
+        }
+        myAppArea.context.stroke();
+        myAppArea.context.fill();
+	}
+	myAppArea.context.strokeStyle = 'red';
+	myAppArea.context.lineWidth=5;
+}
